b825a1dd50a7f98642d0a9f025fd99e8c7697491
LouisLin <louis.lin.yw@gmail.com>
2025-04-11 23:45:49 +0800

    [rp2040] Using the queue to implement multitask Description: 	Using the queue to implement multitask, and show message in the console.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,44 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdio.h>
+#include <queue.h>
 #include "pico/stdlib.h"
 
 #define LED_PIN 22
 
-void led_task()
+static QueueHandle_t xQueue = NULL;
+
+void led_task(void *pvParameters)
 {   
-    //const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    // const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    uint uIvalueToSend = 0;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
+        uIvalueToSend = 1;
+        xQueueSend(xQueue, &uIvalueToSend, 0U);
         vTaskDelay(100);
+
         gpio_put(LED_PIN, 0);
+        uIvalueToSend = 0;
+        xQueueSend(xQueue, &uIvalueToSend, 0U);
+
         vTaskDelay(100);
+    }
+}
+
+void usb_task(void *pvParameters)
+{
+    uint uIvalueReceiveValue = 0;
+    while(1){
+        xQueueReceive(xQueue, &uIvalueReceiveValue, portMAX_DELAY);
+
+        if(uIvalueReceiveValue == 1){
+            printf("LED is On!\n");
+        }else if(uIvalueReceiveValue == 0){
+            printf("LED is Off!\n");
+        }
     }
 }
 
@@ -22,7 +46,10 @@ int main()
 {
     stdio_init_all();
 
+    xQueue = xQueueCreate(1, sizeof(uint));
+
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(usb_task, "USB_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
