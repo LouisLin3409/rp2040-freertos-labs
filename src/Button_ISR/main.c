c427b6081eea2bb8be3a15dbce48c60212c6b3b5
LouisLin <louis.lin.yw@gmail.com>
2025-04-27 21:16:49 +0800

    [rp2040] Using timer to trigger led blinking to prevent hardware buttons bouncing
    Description:
    	Using a button to trigger the ISR may cause a short burst of triggers if no additional hardware is applied.
    	So this modification uses Timer trigger to avoid this problem.
    	The process is as follows:
    			Push Button -> Button_callback_funs and set a timer -> (Time's up)Send blinking freqyency value to the Queue -> Blinking task receive value from Queue-> Blinking LED

--- src/Button_ISR/main.c
+++ src/Button_ISR/main.c
@@ -2,6 +2,7 @@
 #include "task.h"
 #include <stdio.h>
 #include <queue.h>
+#include <timers.h>
 #include "pico/stdlib.h"
 
 #define LED_PIN_GREEN 22
@@ -12,6 +13,7 @@
 #define BLINK_SLOW 2
 
 static QueueHandle_t xQueue = NULL;
+static TimerHandle_t xDebounceTimer = NULL;
 
 void init_button()
 {
@@ -28,10 +30,30 @@ void init_led(uint gpio_led)
 
 void button_callback(uint gpio, uint32_t events)
 {
-   static uint frequency_level = 0;
-   xQueueSendFromISR(xQueue, &frequency_level, NULL);
-   frequency_level++;
-   frequency_level = frequency_level % 3;
+    // Declare a flag to determine if a highier priority task was worken
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    /* Start the debounce timer from within the ISR
+     * This will delay the actual button processing to avoid bouncing effects 
+     */
+    xTimerStartFromISR(xDebounceTimer, &xHigherPriorityTaskWoken);
+
+    /* If starting the timer caused a higher priority task to become ready,
+     * request a context switch to ensure it executes immediately after the ISR
+     */
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+void debounce_timer_callback(TimerHandle_t xTimer){
+    static uint32_t frequence_level = 0;
+
+    if(gpio_get(BUTTON_PIN) == 0){
+        if(xQueueSend(xQueue, &frequence_level, 0)){
+            printf("[%s]Send level%d to Queue\n\n", __FUNCTION__, frequence_level);
+            frequence_level++;
+            frequence_level = frequence_level % 3;
+        }
+    }
 }
 
 void led_task_Green(void *pvParameters)
@@ -68,6 +90,8 @@ int main()
     stdio_init_all();
     init_button();
     xQueue = xQueueCreate(1, sizeof(uint));
+    xDebounceTimer = xTimerCreate("DebounceTimer", pdMS_TO_TICKS(30), pdFALSE, NULL, debounce_timer_callback);
+    
     gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
 
     xTaskCreate(led_task_Green, "LED_Task_Green", 256, NULL, 1, NULL);
